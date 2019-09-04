dc455d0763d1bba6e423ead7aab220ea8c100a0b
ql <ql@1705ha.com>
2019-09-04 14:05:53 +0800

    修改的

---
+++ src/main/java/com/bw/entity/Student.java
@@ -0,0 +1,11 @@
+package com.bw.entity;
+
+/**
+ * @QL
+ *2019Äê8ÔÂ29ÈÕ
+ */
+public class Student {
+	public static void main(String[] args) {
+		System.out.println("xxxxxxxxxxxx");
+	}
+}
---
+++ src/test/java/com/bw/test/MavenTest.java
@@ -0,0 +1,19 @@
+package com.bw.test;
+
+import org.junit.Test;
+
+/**
+ * @QL
+ *2019年8月30日
+ */
+public class MavenTest {
+	@Test
+	public void test1(){
+		System.out.println("挺好的.....");
+	}
+	
+	@Test
+	public void test2(){
+		System.out.println("xxxxxxxxxxxxxxxxxx");
+	}
+}
